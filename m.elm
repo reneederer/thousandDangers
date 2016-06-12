9b1cc8aaa6e881ccb02ec957334f728ec07e90df
reneederer <reneederer@arcor.de>
2016-06-12 18:25:31 +0200

    Deleted files

--- m.elm
+++
@@ -1,6 +0,0 @@
-import Json.encode exposing (..)
-
-
-main = toHtml <|
-        object [ ("id", Json.Encode.int a.id)
-           , ("title", Json.Encode.string ("22")) ]
--- src/m.elm
+++
@@ -1,8 +0,0 @@
-import Json.Encode exposing (..)
-import Html exposing (..)
-
-
-main = 
-        let x = Json.Encode.object [ ("id", Json.Encode.int 23), ("title", Json.Encode.string ("22")) ]
-        in
-            div [] [text <| toString x]
